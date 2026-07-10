6f6b0c8a9860ff2c4331714acebca631bbb573b2
Silvio <silvio.oliveira347@gmail.com>
2026-07-09 21:57:31 -0300

    auto compile and secrets

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,12 @@
-// src/index.ts
 import { onRequest } from "firebase-functions/v2/https";
 import { buildApp } from "./app";
 
 const app = buildApp();
 
 export const api = onRequest(
+  {
+    secrets: ["PUBLIC_API_KEY"],
+  },
   async (req, res) => {
     await app.ready();
     app.server.emit("request", req, res);
